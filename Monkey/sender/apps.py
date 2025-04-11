a5c33d78539eaec1642248f525c627c83b34a95c
Durgesh Singh <durgesh.singh90@gmail.com>
2025-04-11 23:43:37 +0100

    CleanUp

--- Monkey/sender/apps.py
+++ Monkey/sender/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class RrnSearchConfig(AppConfig):
+class SenderConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'rrn_search'
+    name = 'sender'
