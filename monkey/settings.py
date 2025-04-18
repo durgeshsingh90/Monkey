c80600ca8bf1dc5e90c3879bdbff750e24172349
Durgesh Singh <durgesh.singh90@gmail.com>
2025-04-18 05:04:43 +0100

    Ignore all .log files

--- monkey/settings.py
+++ monkey/settings.py
@@ -102,7 +102,7 @@ ROOT_URLCONF = 'monkey.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['html'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
