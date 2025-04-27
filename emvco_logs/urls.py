bb59f5d6837f16df62145a073975d7bddff9eb21
Durgesh Singh <durgesh.singh90@gmail.com>
2025-04-27 13:46:03 +0100

    loading screen_EMVCO

--- emvco_logs/urls.py
+++ emvco_logs/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'emvco_logs'
+
 urlpatterns = [
     path('', views.index, name='emvco_logs_index'),
     path('upload/', views.upload_file, name='upload_file'),
