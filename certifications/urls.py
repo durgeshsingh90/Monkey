a74a070563a20fb59fe4b8f4dc774a288c2675c5
Durgesh Singh <durgesh.singh90@gmail.com>
2025-04-29 09:16:44 +0100

    certifications html

--- certifications/urls.py
+++ certifications/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
-    ]
+    path('', views.certifications_index, name='certifications_index'),
+    path('get_structure/', views.get_structure, name='get_structure'),
+    path('get_testcase_data/', views.get_testcase_data, name='get_testcase_data'),
+]
