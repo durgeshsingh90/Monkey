6e5287d3caf94af296f35d7afd111b1817bae82a
Durgesh Singh <durgesh.singh90@gmail.com>
2025-05-19 02:58:29 +0100

    housekeeping

--- certifications/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.certifications_index, name='certifications_index'),
-    path('get_structure/', views.get_structure, name='get_structure'),
-    path('get_testcase_data/', views.get_testcase_data, name='get_testcase_data'),
-]
